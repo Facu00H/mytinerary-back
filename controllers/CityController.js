aa826ace0aac02e7ffb5972be7ae4272122a0d8d
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-08-30 22:24:15 -0300

    added read, readAll, uptade and delete methods to CityController.js

--- controllers/CityController.js
+++ controllers/CityController.js
@@ -7,17 +7,61 @@ const CityController = {
             await new City(req.body).save() //req.body tiene que tener todas las variables antes descritas
             res.status(201).json({ message: 'City created', succsess: true });
         } catch (error) {
-            res.status(400).json({ message: 'City not created', succsess: false });
+            console.log(error);
+            res.status(400).json({ message: 'City not created', success: false });
+        }
+    },
+
+    readAll: async (req, res) => {
+        let cities = req.params
+        try {
+            cities = await City.find();
+            res.status(200).json({ message: 'Showing all cities', response: cities, success: true })
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({ message: 'Error', success: false })
         }
     },
 
     read: async (req, res) => {
+        const { id } = req.params;
+        try {
+            let city = await City.findOne({ _id:id });
+            if (city) {
+                res.status(200).json({ message: 'City found', response: city, success: true });
+            } else {
+                res.status(404).json({ message: 'City not found', succsess: false });
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({ message: 'Error', succsess: false });
+        }
+    },
+
+    update: async (req, res) => {
+        const { id } = req.params;
         const { city, country, photo, population, fundation } = req.body;
         try {
-            await City(req.body);
-            res.status(200).json({ message: 'City found', succsess: true });
+            let city = await City.findByIdAndUpdate({ _id:id }, req.body );
+            if (city) {
+                res.status(200).json({ message: 'City updated', response: city, success: true });
+            } else {
+                res.status(404).json({ message: 'City not found', success: false });
+            }
         } catch (error) {
-            res.status(404).json({ message: 'City not found', succsess: false });
+            console.log(error);
+            res.status(400).json({ message: 'Error', success: false });
+        }
+    },
+
+    destroy: async (req, res) => {
+        const { id } = req.params;
+        try {
+            let city = await City.findOneAndDelete({ _id:id });
+            res.status(200).json({ message: 'City deleted', response: city, success: true });
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({ message: 'Error', success: false });
         }
     }
 }
