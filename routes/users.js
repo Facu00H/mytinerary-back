b6300d52aab666493c1988b0f5537a83773a24d2
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-09-05 19:56:48 -0300

    users route defined

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { create } = require('../controllers/Users'); //traigo los metodos del controller
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+router.post('/', create); //cuando haga un post a /, ejecuta el metodo create del controller
 module.exports = router;
