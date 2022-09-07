a7db0b245d39845d181a05beb18ee9374374c5ff
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-09-07 17:41:21 -0300

    routes added

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { create, readAll } = require('../controllers/Comments'); //traigo los metodos del controller
+const { create, readFromItinerary, readFromItineraryQuery } = require('../controllers/Comments'); //traigo los metodos del controller
 
 router.post('/', create); //cuando haga un post a /, ejecuta el metodo create del controller
-router.get('/', readAll); //cuando haga un get a /, ejecuta el metodo readAll del controller
+router.get('/query', readFromItineraryQuery);
+router.get('/itinerary/:id', readFromItinerary);
 module.exports = router;
