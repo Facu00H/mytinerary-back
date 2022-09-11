59c50c2df507aceff1162061eae2e7f3a2360124
Facu00H <faisher00@gmail.com>
2022-09-11 20:18:14 -0300

    add response.id to create

--- controllers/Users.js
+++ controllers/Users.js
@@ -5,8 +5,8 @@ const usersController = {
     create: async (req, res) => {
         const { name, lastName, mail, password, photo, country } = req.body;
         try {
-            await new User(req.body).save()
-            res.status(201).json({ message: 'User created', succsess: true });
+            let user = await new User(req.body).save()
+            res.status(201).json({ message: 'User created', succsess: true, id: user._id });
         } catch (error) {
             console.log(error);
             res.status(400).json({ message: 'User not created', success: false });
