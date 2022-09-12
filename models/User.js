e15198ea5fcee81c27017bbe557734394c21e6b5
Facu00H <faisher00@gmail.com>
2022-09-12 17:29:11 -0300

    change password to array of strings

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     lastName: {type: String, required: true},
     mail: {type: String, required: true},
-    password: {type: String, required: true},
+    password: [{type: String, required: true}],
     photo: {type: String, required: true},
     country: {type: String, required: true},
     role: {type: String, required: true},
