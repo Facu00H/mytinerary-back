ef524bddde638d6999b36b2e99e1868b9f71d942
Facu00H <faisher00@gmail.com>
2022-09-12 18:43:31 -0300

    remove router readAll

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { create, readAll } = require('../controllers/Users'); //traigo los metodos del controller
+const { signUp } = require('../controllers/Users'); //traigo los metodos del controller
 
-router.post('/', create); //cuando haga un post a /, ejecuta el metodo create del controller
-router.get('/', readAll);
+router.post('/signup', signUp); //cuando haga un post a /, ejecuta el metodo create del controller
 module.exports = router;
