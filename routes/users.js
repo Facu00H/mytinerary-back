37a203ee81f7f26ffc28fcbc2c4e7d700437676f
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-09-21 19:24:18 -0300

    users routes updated

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { signUp, signIn, signOut, verifyMail } = require('../controllers/Users'); //traigo los metodos del controller
+const passport = require('../config/passport');
 
 router.post('/signup', signUp);
 router.get('/verify/:code',verifyMail)
 router.post('/signin', signIn);
-router.post('/signout', signOut); //cuando haga un post a /, ejecuta el metodo create del controller
+router.post('/signout', signOut);
 module.exports = router;
